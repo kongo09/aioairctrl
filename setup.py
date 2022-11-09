20b858664752b84d47787a6f860cc986093582cb
Rimon Hanna <rimon.hanna@live.com>
2022-11-09 18:08:22 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,20 +5,20 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="aioairctrl",
-    version="0.2.3",
+    version="0.2.6",
     description="Library for controlling Philips air purifiers (using encrypted CoAP)",
     long_description=long_description,
     author="betaboon",
-    url="https://github.com/kongo09/aioairctrl",
+    url="https://github.com/rimonhanna/aioairctrl",
     project_urls={
-        "Bug Tracker": "https://github.com/kongo09/aioairctrl/issues",
+        "Bug Tracker": "https://github.com/rimonhanna/aioairctrl/issues",
     },
     license="MIT",
     package_dir={"": "."},
     packages=setuptools.find_packages(),
     install_requires=[
         "pycryptodomex",
-        "aiocoap>=0.4.1, <0.5",
+        "aiocoap>=0.4.4, <0.5",
     ],
     python_requires=">=3.6",
     entry_points={
